506a9253f0a07751b5dee3ad3f4d04c8cd203786
dragoocho <deagoocho@gmail.com>
2020-12-22 18:53:23 +0900

    이미지 슬라이더 만들기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
+import Axios from "axios";
+import { Icon, Col, Card, Row, Carousel } from "antd";
+import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
+  const [Products, setProducts] = useState([]);
+
+  useEffect(() => {
+    Axios.post("/api/product/products").then((response) => {
+      if (response.data.success) {
+        setProducts(response.data.productInfo);
+      } else {
+        alert("상품들을 가져오는데 실패했습니다.");
+      }
+    });
+  }, []);
+
+  const renderCards = Products.map((product, index) => {
+    console.log("product", product);
+
     return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`$${product.price}`} />
+          <Meta description={product.description} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere <Icon type="rocket" />{" "}
+        </h2>
+      </div>
+
+      {/* Filter */}
+
+      {/* Search */}
+
+      {/* Cards */}
+
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>더보기</button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -62,7 +62,6 @@ function UploadProductPage(props) {
       if (response.data.success) {
         alert("상품 업로드에 성공 했습니다.");
         props.history.push("/");
-        console.log(Images.Array);
       } else {
         alert("상품 업로드에 실패 했습니다.");
       }
