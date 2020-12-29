1dc78449007becee77e483febdedd896b0afd3d8
dragoocho <deagoocho@gmail.com>
2020-12-29 12:41:04 +0900

    Product Image 컴포넌트 만들기

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,8 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ProductImage from "./Sections/ProductImage";
+import ProductInfo from "./Sections/ProductInfo";
+import { Col, Row } from "antd";
 
 function DetailProductPage(props) {
   const productId = props.match.params.productId;
+
+  const [Product, setProduct] = useState({});
 
   useEffect(() => {
     axios
@@ -10,13 +15,33 @@ function DetailProductPage(props) {
       .then((response) => {
         if (response.data.success) {
           console.log("response.data", response.data);
+          setProduct(response.data.product[0]);
         } else {
           alert("상세정보 가져오기를 실패했습니다.");
         }
       });
   }, []);
 
-  return <div>DetailProductPage dfgzg</div>;
+  return (
+    <div style={{ width: "100%", padding: "3rem 4rem" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1>{Product.title}</h1>
+      </div>
+
+      <br />
+
+      <Row gutter={[16, 16]}>
+        <Col lg={12} sm={24}>
+          {/* ProductImage */}
+          <ProductImage detail={Product}/>
+        </Col>
+        <Col lg={12} sm={24}>
+          {/* productInfo */}
+          <ProductInfo />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default DetailProductPage;
